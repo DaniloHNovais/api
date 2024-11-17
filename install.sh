251f10d646143aca99581638e2958219aa348702
DaniloHNovais <89391953+DaniloHNovais@users.noreply.github.com>
2024-11-17 16:39:00 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -246,8 +246,9 @@ function create_cw_user() {
 function configure_rvm() {
   create_cw_user
 
-  gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  gpg --keyserver hkps://keys.openpgp.org --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
   curl -sSL https://get.rvm.io | bash -s stable
   adduser chatwoot rvm
 }
